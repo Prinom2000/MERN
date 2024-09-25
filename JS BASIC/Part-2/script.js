0dbad483c0117397ef1771730617fae772a4bed5
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-09-25 23:52:28 +0600

    Add files via upload

---
+++ JS BASIC/Part-2/script.js
@@ -0,0 +1,60 @@
+// alert("hello....!");
+
+// confirm("Are U Sure........!");
+
+// console.log("Prionom");
+// console.log(3+4);
+
+// console.error("NO NO NO");
+
+// console.warn("Sabdhan");
+
+// console.log("I am prinom. \n\tI Love CSE")
+
+// oparetor
+// var x=5;
+// var y=3;
+// x *=y;
+// x /=y;
+// x %=y;
+// console.log(x);
+
+
+// const n=5;
+// if(n%2===0){
+//     console.log("Even");
+// }
+// if(n%6 !=0){
+//     console.log("odd");
+// }
+
+
+// const m=30;
+// if(m>100 || m<0){
+//     console.log("Vul");
+// }
+
+
+// make a calculator with user input:
+const n1=Number(prompt("Enter the 1st Number:"));  //const is use for only constant.
+const n2=Number(prompt("Enter the 2nd Number:"));
+var s="SUM";
+
+
+console.log(`${s}: ${n1}+${n2}=${n1+n2}`);  // for printing: `${}`
+console.log(`SUB: ${n1}-${n2}=${n1-n2}`);
+console.log(`${n1}*${n2}=${n1*n2}`);
+console.log(`${n1}/${n2}=${n1/n2}`);
+console.log(`${n1}%${n2}=${n1%n2}`);
+
+console.log($n1);
+
+
+
+
+
+
+
+
+
+
